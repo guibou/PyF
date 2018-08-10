865d22c6f195ab643175bb9d3659bda5a0aac2e5
Guillaume Bouchard <guillaume.bouchard@tweag.io>
2018-08-10 22:41:52 +0200

    Simplify the cabal and default.nix
    - No more pinned repository. I'm develloping, I don't really care
    - Cabal now include `python` as build-tool
    - Release 0.6.1.1

--- default.nix
+++ default.nix
@@ -1,7 +1,4 @@
-{ pkgs ? import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/ee28e35ba37ab285fc29e4a09f26235ffe4123e2.tar.gz) {} }:
-with pkgs.haskellPackages;
-(developPackage {
+with import <nixpkgs> {};
+haskellPackages.developPackage {
   root = ./.;
-}).overrideAttrs (oldAttrs: {
-    buildInputs = oldAttrs.buildInputs ++ [pkgs.python3];
-    })
+}
