f6abd3c9d70bd90f653ccc55620100eb5c528fd7
Guillaume Bouchard <guillaume.bouchard@tweag.io>
2019-08-06 16:28:39 +0200

    Update nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
 let
-  sha256 = "093n9ci000vlhz700mrkdv7jnx6szgmggl0x0wbr1d0yrg2y7q43";
-  rev = "f612ab79002c546d668c4f4ee3d5321fc814b838";
+  sha256 = "095l3gqpyh1c6xijhin2bry8qq2w7yzidiiaadyzzwjn6vvqm9xi";
+  rev = "002b853782e";
 in
 import (fetchTarball {
   inherit sha256;
