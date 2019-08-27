e3d20a79a3493c08748b4813a030e321ef885528
Guillaume Bouchard <guillaume.bouchard@tweag.io>
2019-08-27 15:45:39 +0200

    Add golden test in cabal manifest
    - Tests are renamed to test/golder/*.golden to work around cabal glob
      syntax limitations.
    - use buildFromSdist in nix to ensure that CI test the sdist

--- default.nix
+++ default.nix
@@ -1,16 +1,20 @@
 { nixpkgs ? ./nixpkgs.nix }:
 with import nixpkgs {};
-haskellPackages.developPackage {
-  name = "PyF";
+rec {
+  pyf = haskellPackages.developPackage {
+    name = "PyF";
 
-  # Filter temp files, git files, .ghc.environment and .nix files
-  # which are not part of the build
-  root = lib.sources.cleanSourceWith {
-    filter = name: type: let baseName = baseNameOf (toString name); in
-    !(lib.hasPrefix ".ghc.environment." baseName) && (baseName != "default.nix");
-    src = lib.sources.cleanSource ./.;
+    # Filter temp files, git files, .ghc.environment and .nix files
+    # which are not part of the build
+    root = lib.sources.cleanSourceWith {
+      filter = name: type: let baseName = baseNameOf (toString name); in
+      !(lib.hasPrefix ".ghc.environment." baseName) && (baseName != "default.nix");
+      src = lib.sources.cleanSource ./.;
+    };
+
+    overrides = self : super : {
+    };
   };
 
-  overrides = self : super : {
-  };
+  pyf-sdist = haskell.lib.buildFromSdist pyf;
 }
