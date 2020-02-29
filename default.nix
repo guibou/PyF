e453ea6a025a002d05b0d37bc9ce182051a95e95
Guillaume Bouchard <guillaume.bouchard@tweag.io>
2020-02-29 14:18:53 +0100

    A huge batch of CI and nix cleaning
    CI:
      - build ghc 8.6 and 8.8 with nix and stack
      - more coherent cache handling
    Nix:
      - ormolu and hlint linters
      - source is now a whitelist

--- default.nix
+++ default.nix
@@ -1,30 +1,76 @@
-{ nixpkgs ? ./nixpkgs.nix }:
-with import nixpkgs {};
+{ pkgs ? import ./nixpkgs.nix {} }:
+with pkgs;
 rec {
-  pyfBuilder = hPkgs: hPkgs.developPackage {
-    name = "PyF";
+  inherit pkgs;
 
-    # Filter temp files, git files, .ghc.environment and .nix files
-    # which are not part of the build
-    root = lib.sources.cleanSourceWith {
-      filter = name: type: let baseName = baseNameOf (toString name); in
-         !(lib.hasPrefix ".ghc.environment." baseName) && (builtins.match ".+\\.nix" (toString name) == null);
-      src = lib.sources.cleanSource ./.;
-    };
+  # Explicit list of used files. Else there is always too much and
+  # cache is invalidated.
+  sources = lib.sourceByRegex ./.  [
+    "PyF\.cabal$"
+    ".*\.hs$"
+    ".*\.md$"
+    ".*\.golden$"
+    "src"
+    "app"
+    "src/PyF"
+    "src/PyF/Internal"
+    "test"
+    "test/failureCases"
+    "test/golden"
+    "LICENSE"
+  ];
 
-    overrides = self : super : {
-      megaparsec = super.callHackageDirect {
-        pkg = "megaparsec";
-        ver = "8.0.0";
-        sha256 = "1bk4jsa69maryj97jcvxxc211icvnkr21xrj2bqq9ddfizkq5lg0";
-      } {};
-    };
-  };
+  pyfBuilder = hPkgs: haskell.lib.buildFromSdist ((hPkgs.override {
+    overrides = self: super : {
+        megaparsec = super.callHackageDirect {
+          pkg = "megaparsec";
+          ver = "8.0.0";
+          sha256 = "1bk4jsa69maryj97jcvxxc211icvnkr21xrj2bqq9ddfizkq5lg0";
+        } {};
+    };}).callCabal2nix "PyF" sources {});
 
   pyf_86 = pyfBuilder haskell.packages.ghc865;
   pyf_88 = pyfBuilder haskell.packages.ghc881;
 
   pyf = pyf_88;
 
-  pyf-sdist = haskell.lib.buildFromSdist pyf;
+  # Run hlint on the codebase
+  hlint = runCommand "hlint-krank" {
+    nativeBuildInputs = [haskellPackages.hlint];
+  }
+  ''
+  cd ${sources}
+  hlint .
+  mkdir $out
+  '';
+
+  # Run ormolu on the codebase
+  # Fails if there is something to format
+  ormolu = runCommand "ormolu-krank" {
+    nativeBuildInputs = [haskellPackages.ormolu];
+  }
+  ''
+  cd ${sources}
+  ormolu --mode check $(find -name '*.hs')
+  mkdir $out
+  '';
+
+  hlint-fix = mkShell {
+    nativeBuildInputs = [haskellPackages.hlint git haskellPackages.apply-refact];
+    shellHook = ''
+      for file in $(git ls-files | grep '\.hs$')
+      do
+        hlint  --refactor --refactor-options='-i' $file
+      done
+      exit 0
+    '';
+  };
+
+  ormolu-fix = mkShell {
+    nativeBuildInputs = [haskellPackages.ormolu git];
+    shellHook = ''
+      ormolu --mode inplace $(git ls-files | grep '\.hs$')
+      exit 0
+    '';
+  };
 }
