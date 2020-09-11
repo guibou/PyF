ffb2f97013365f101f53697aa9edff4a78c708c6
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2020-09-11 13:06:02 +0200

    Bump megaparsec requirement
    For stackage update, see
    
    https://github.com/commercialhaskell/stackage/issues/5632

--- default.nix
+++ default.nix
@@ -23,7 +23,11 @@ rec {
   pyfBuilder = hPkgs: haskell.lib.buildFromSdist (hPkgs.callCabal2nix "PyF" sources {});
 
   pyf_86 = pyfBuilder haskell.packages.ghc865;
-  pyf_88 = pyfBuilder haskell.packages.ghc884;
+  pyf_88 = pyfBuilder (haskell.packages.ghc884.override {
+    overrides = self: super: with hasell.lib; {
+      megaparsec = super.megaparsec_9_0_0;
+    };
+  });
   pyf_810 = pyfBuilder (haskell.packages.ghc8102.override {
     overrides = self: super: with haskell.lib; {
       th-expand-syns = doJailbreak super.th-expand-syns;
