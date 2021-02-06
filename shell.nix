99721963cfa6fc8e298e6a5a445be0af9d47d515
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2021-02-06 12:39:55 +0100

    Haskell language server in the nix shell

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 with import ./. {};
 pyf.env.overrideAttrs (old: {
    # The haskell environment does not come with cabal-install
-   nativeBuildInputs = old.nativeBuildInputs ++ [pkgs.cabal-install pkgs.python3];
+   nativeBuildInputs = old.nativeBuildInputs ++ [pkgs.cabal-install pkgs.python3 pkgs.haskellPackages.haskell-language-server ];
 })
