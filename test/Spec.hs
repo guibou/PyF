97d2707f108b3232ade16c25d67fdcdc1878f323
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2021-06-01 21:14:40 +0200

    Rework the strip algo
    The algorithm now preserve final spaces / line returns.
    
    This is intended so composition preserves line returns, such as:
    
    ```
    [fmt|
       - a
       - b
    |] <> [fmt|
       - c
       - d
    |]
    ```
    
    Will returns `- a\n- b\n- c\n- d\n`.

--- test/Spec.hs
+++ test/Spec.hs
@@ -393,24 +393,69 @@ yeah\
 -- [fmt|{fst ((,2) 1)}|] `shouldBe` "1"
 
   describe "multiline trimming" $ do
+    it "works with overloading" $ do
+      [PyF.Trimmed.fmt|hello|] `shouldBe` ("hello" :: Data.Text.Text)
+    it "overloading in overloading" $ do
+      let foo = [PyF.Trimmed.fmt|hello {10}|]
+      -- foo `shouldBe` "hello 10"
+      [PyF.Trimmed.fmt|biz {foo} goodbye|] `shouldBe` ("biz hello 10 goodbye" :: Data.Text.Text)
+    it "do not fail on trailing ignore line return" $ do
+      [PyF.Trimmed.fmt|
+      hello\
+
+      |] `shouldBe` "hello\n"
+    it "do not take too much indent in account" $ do
+      [PyF.Trimmed.fmt|
+      hello
+      - a
+        - b
+      - c
+      |] `shouldBe` "hello\n- a\n  - b\n- c\n"
+    it "works with empty lines" $ do
+      [PyF.Trimmed.fmt|
+      hello
+
+
+      |] `shouldBe` "hello\n\n\n"
+    it "works with empty last lines" $ do
+      [PyF.Trimmed.fmt|
+      hello
+
+
+|] `shouldBe` "hello\n\n\n"
     it "works" $ do
-      [PyF.Trimmed.fmt|   hello
+      [PyF.Trimmed.fmt|
+                  hello
                   - a
                    - b
 
                   - c
       |]
-        `shouldBe` "hello\n- a\n - b\n\n- c"
+        `shouldBe` "hello\n- a\n - b\n\n- c\n"
     it "works with replacement" $ do
-      [PyF.Trimmed.fmt|  hello
+      [PyF.Trimmed.fmt|
+                  hello
                   - a
                    - {pi:.2}|]
         `shouldBe` "hello\n- a\n - 3.14"
+    it "Do not ignore not indented lines" $ do
+      [PyF.Trimmed.fmt|  hello
+- a
+ - {pi:.2}|]
+        `shouldBe` "  hello\n- a\n - 3.14"
 
     it "works with multiline" $ do
-      [PyF.Trimmed.fmt|hello
+      [PyF.Trimmed.fmt|
+                  hello
                   - a
                    - {
                 2 + 2
                    :d}|]
         `shouldBe` "hello\n- a\n - 4"
+
+    it "behaves well with escaped first line" $ do
+      [PyF.Trimmed.fmt|\
+                  - a
+                  - b
+                  |]
+        `shouldBe` "- a\n- b\n"
