039e4775febb3b97f998c287b5d582c5022c0143
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2022-02-12 20:09:23 +0300

    Add support for overloaded labels

--- src/PyF/Internal/Meta.hs
+++ src/PyF/Internal/Meta.hs
@@ -195,6 +195,9 @@ toExp d (Expr.ArithSeq _ _ e) = TH.ArithSeqE $ case e of
   (FromThen a b) -> TH.FromThenR (toExp d $ unLoc a) (toExp d $ unLoc b)
   (FromTo a b) -> TH.FromToR (toExp d $ unLoc a) (toExp d $ unLoc b)
   (FromThenTo a b c) -> TH.FromThenToR (toExp d $ unLoc a) (toExp d $ unLoc b) (toExp d $ unLoc c)
+-- It's not quite clear what to do in case when overloaded syntax is
+-- enabled thus match on Nothing
+toExp _ (HsOverLabel _ Nothing lbl) = TH.LabelE (unpackFS lbl)
 toExp dynFlags e = todo "toExp" (showSDocDebug dynFlags . ppr $ e)
 
 todo :: (HasCallStack, Show e) => String -> e -> a
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,9 +3,13 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
@@ -23,6 +27,9 @@ import qualified Data.Ratio
 import qualified Data.Text
 import qualified Data.Text.Lazy
 import qualified Data.Time
+import Data.Proxy (Proxy(..))
+import GHC.TypeLits (Symbol, KnownSymbol, symbolVal)
+import GHC.OverloadedLabels
 import PyF
 import SpecCustomDelimiters
 import SpecUtils
@@ -59,6 +66,19 @@ type instance PyFClassify (FooFloating t) = 'PyFFractional
 type instance PyFClassify FooIntegral = 'PyFIntegral
 
 type instance PyFClassify FooDefault = 'PyFString
+
+-- Data type to test overloaded labels
+data V (a :: Symbol) = V
+
+instance KnownSymbol a => Show (V a) where
+  show V = "V=" ++ symbolVal (Proxy @a)
+
+instance (a ~ a') => IsLabel a (V a') where
+  fromLabel = V
+
+showV :: KnownSymbol a => V a -> String
+showV = show
+
 
 spec :: Spec
 spec = do
@@ -302,6 +322,8 @@ yeah\
       [fmt|hello {show @_ 10}|] `shouldBe` "hello 10"
     it "parses BinaryLiterals" $
       [fmt|hello {0b1111}|] `shouldBe` "hello 15"
+    it "OverloadedLabels works" $
+      [fmt|{showV #abc}|] `shouldBe` "V=abc"
   describe "custom types" $ do
     it "works with integral" $
       [fmt|{FooIntegral 10:d}|] `shouldBe` "10"
